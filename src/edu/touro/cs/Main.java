addc20ce81aa4738fdd781ec3b098b071918b705
Jonathan Robinson <Jonathan.Robinson@turkprime.com>
2021-01-28 08:20:43 -0500

    mouse capture and paint

---
+++ src/edu/touro/cs/Main.java
@@ -0,0 +1,12 @@
+package edu.touro.cs;
+
+public class Main {
+    public static void main(String[] args) {
+        new MyWindow();
+    }
+}
+
+
+
+
+
--- src/edu/touro/cs/Temp.java
+++ src/edu/touro/cs/Temp.java
@@ -1,13 +1,19 @@
 package edu.touro.cs;
 
 import javax.swing.*;
-import java.awt.*; // abstract windowing toolkit
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.util.LinkedList;
+
+
 
 class MyWindow extends JFrame {
     private JTextField textField1, textField2;
     private JLabel sumLabel;
+    private java.util.List<Point> pointList = new LinkedList<>();
 
     MyWindow() {
         this.setTitle("First 364 GUI!!");
@@ -15,41 +21,68 @@ class MyWindow extends JFrame {
 
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        this.getContentPane().setBackground( Color.BLACK);
-        this.getContentPane().setLayout(new FlowLayout());
+        JPanel topPanel = new JPanel();
 
-        JButton button = new JButton("ADD");
-        this.getContentPane().add(button);
+        JButton button = new JButton("ADD"); // event source
+        topPanel.add(button);
 
-        // registerimng the event listener
-        button.addActionListener(new MyActionListener());
+        // registering the event listener
+        ActionListener al = new MyActionListener(); // 2
+        button.addActionListener(al); // 3
 
         textField1 = new JTextField(10);
-        this.getContentPane().add(textField1);
+        topPanel.add(textField1);
         textField2 = new JTextField(10);
-        this.getContentPane().add(textField2);
+        topPanel.add(textField2);
 
         sumLabel = new JLabel("Sum will go here");
-        this.getContentPane().add(sumLabel);
+        topPanel.add(sumLabel);
+
+        this.add(topPanel, BorderLayout.NORTH);
+
+        this.addMouseMotionListener( new MyMouseMotionListner());
+
 
         this.setVisible(true);
     }
 
-    // event handler
+    @Override
+    public void paint(Graphics g)
+    {
+        super.paint(g);
+
+        g.setColor( new Color(255, 55,87));
+        for(Point p: pointList)
+        {
+            g.fillOval(p.x, p.y, 4, 4);
+        }
+
+    }
+    class MyMouseMotionListner implements MouseMotionListener{
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            System.out.println(String.format("Dragged at x:%d, y:%d", e.getX(), e.getY() ));
+            MyWindow.this.pointList.add( e.getPoint());
+            repaint();
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            System.out.println(String.format("Moved at x:%d, y:%d", e.getX(), e.getY() ));
+        }
+    }
+    // event handler in an (nonstatic) inner class
     private class MyActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
+            JButton buttonSource = (JButton) e.getSource();
             String text1 = textField1.getText();
             String text2 = textField2.getText();
 
-            sumLabel.setText( (Integer.parseInt(text1) + Integer.parseInt(text2)) + "");
+            String display = (Integer.parseInt(text1) + Integer.parseInt(text2)) + "";
 
+            sumLabel.setText( display );
+            buttonSource.setText(display);
         }
-    }
-}
-
-public class Temp {
-    public static void main(String[] args) {
-        new MyWindow();
     }
 }
